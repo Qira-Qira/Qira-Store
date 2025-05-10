aebeee4754d53f9df498790b9c9f9860fbb9eb19
Qira-Qira <nawafariq3@gmail.com>
2025-05-10 13:17:34 +0700

    Update

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import { Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={"4"}>
+      <Flex
+        h={"16"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22", sm: "28" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient={"linear(to-l, #a31621, #fcf7fb)"}
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Qira Store 🛒</Link>
+        </Text>
+
+        <HStack spacing={"4"} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <CiSquarePlus fontSize={"20"}/>
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default Navbar;
