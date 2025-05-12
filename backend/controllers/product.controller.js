3ce5d0a4168337a86ce8e0d1e882302ecd0986af
Qira-Qira <nawafariq3@gmail.com>
2025-05-12 15:43:25 +0700

    Finish

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,62 +2,62 @@ import mongoose from "mongoose";
 import Product from "../models/product.model.js";
 
 
-export const getProducts = async(req,res) => {
-    try{
+export const getProducts = async (req, res) => {
+    try {
         const products = await Product.find({});
-        res.status(200).json({success: true, data: products});
-    }catch(error){
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
         console.log("error in fetching products:", error.message);
-        res.status(500).json({success: false, message: "Server error"});
+        res.status(500).json({ success: false, message: "Server error" });
     }
 };
 
-export const createProduct =  async (req,res) => {
+export const createProduct = async (req, res) => {
     const product = req.body; //user will send this data
 
-    if(!product.name || !product.price || !product.image){
-        return res.status(400).json({success: false, message: "Please provide all fields"});
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
     }
     const newProduct = new Product(product);
-    
-    try{
+
+    try {
         await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    }catch(error){
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
         console.error("Error in create product:", error.message)
-        res.status(500).json({success: false, message: "Server Error"});
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 };
 
-export const updateProduct = async (req,res) => {
-    const {id} = req.params;
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
 
     const product = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({success: false, message: "Invalid product id"});
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid product id" });
     };
 
     try {
-        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
-        res.status(200).json({success: true, data: updatedProduct});
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
     } catch (error) {
-        res.status(500).json({success: false, message: "server error';"})
+        res.status(500).json({ success: false, message: "server error" });
     }
 };
 
-export const deleteProduct = async (req,res) => {
-    const {id} = req.params;
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({success: true, message: "Invalid Product Id"})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: true, message: "Invalid Product Id" })
     };
-    
-    try{
+
+    try {
         await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted"});
-    }catch(error){
+        res.status(200).json({ success: true, message: "Product deleted" });
+    } catch (error) {
         console.log("error in fetching products:", error.message)
-        res.status(500).json({success: false, message: "Server Error"});
+        res.status(500).json({ success: false, message: "Server Error" });
     }
 };
